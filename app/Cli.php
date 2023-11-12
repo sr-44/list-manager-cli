91f9cd5448e4ae93fc0800cd97be49d744569281
sr-44 <mail.sr.44@gmail.com>
2023-11-13 02:57:53 +0500

    use switch-case
    Took 13 minutes

--- app/Cli.php
+++ app/Cli.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -20,29 +21,33 @@ class Cli extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $actions = new Actions($input->getArgument('filename'));
-        $product = $this->ask($input, $output, 'Введите продукт: ');
-        $price = (int)$this->ask($input, $output, 'Введите цену: </>');
-        
         $inputAction = $input->getArgument('action');
-        
-        if ($inputAction === 'add') {
-            if ($actions->add($product, $price)) {
-                $output->writeln('success');
-            }
-        } elseif ($inputAction === 'change') {
-            if ($actions->change($product, $price)) {
-                $output->writeln('success');
-            }
-        } elseif ($inputAction === 'remove') {
-            if ($actions->remove($product)) {
-                $output->writeln('success');
-            }
-        } elseif ($inputAction === 'calculate') {
-            $output->writeln($actions->calculate());
+        switch ($inputAction) {
+            case ActionsType::CALCULATE->value:
+                $output->writeln($actions->calculate());
+                break;
+            case ActionsType::REMOVE->value:
+                $product = $this->ask($input, $output, "Enter product's name: ");
+                if ($actions->remove($product)) {
+                    $output->writeln('Successful removed');
+                }
+                break;
+            case ActionsType::ADD->value:
+            case ActionsType::CHANGE->value:
+                $product = $this->ask($input, $output, "Enter product's name:");
+                $price = (int)$this->ask($input, $output, "Enter product's price:");
+                if ($inputAction === ActionsType::ADD->value && $actions->add($product, $price)) {
+                    $output->writeln('Successful added');
+                } elseif ($inputAction === ActionsType::CHANGE->value && $actions->change($product, $price)) {
+                    $output->writeln('Successful changed');
+                }
+                break;
+            default:
+                throw new RuntimeException('Wrong action, use: add, remove, change, calculate');
         }
-
         return Command::SUCCESS;
     }
+
     private function ask(InputInterface $input, OutputInterface $output, $question)
     {
         $helper = $this->getHelper('question');
