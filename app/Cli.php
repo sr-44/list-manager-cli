1e9bfe4512402758b4f8121f21b57578e5d20553
sr-44 <mail.sr.44@gmail.com>
2023-11-13 02:19:41 +0500

    Cli: added helper method for interactive data retrieval
    Took 7 minutes

--- app/Cli.php
+++ app/Cli.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 class Cli extends Command
 {
@@ -19,12 +20,22 @@ class Cli extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $actions = new Actions($input->getArgument('filename'));
+        $product = $this->ask($input, $output, 'Введите продукт: ');
+        $price = (int)$this->ask($input, $output, 'Введите цену: </>');
+
         if ($input->getArgument('action') === 'add') {
-            if ($actions->add('sdsd', 10)) {
+            if ($actions->add($product, $price)) {
                 $output->writeln('success');
             }
         }
 
         return Command::SUCCESS;
     }
+    private function ask(InputInterface $input, OutputInterface $output, $question)
+    {
+        $helper = $this->getHelper('question');
+        $question = new Question($question);
+        return $helper->ask($input, $output, $question);
+    }
+
 }
