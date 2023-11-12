1c1f5bca23c6887941fe063cda8942a6aa167f60
sr-44 <mail.sr.44@gmail.com>
2023-11-13 04:01:10 +0500

    added validator for check products
    Took 54 minutes

--- app/Cli.php
+++ app/Cli.php
@@ -25,51 +25,90 @@ class Cli extends Command
         $output->getFormatter()->setStyle('success', new OutputFormatterStyle('green', options: ['bold', 'blink']));
         $output->getFormatter()->setStyle('error', new OutputFormatterStyle('red', options: ['bold', 'blink']));
         $inputAction = $input->getArgument('action');
+
+
         try {
             $actions = new Actions($input->getArgument('filename'));
+
             switch ($inputAction) {
                 case ActionsType::CALCULATE->value:
                     $output->writeln($actions->calculate());
                     break;
+
                 case ActionsType::REMOVE->value:
-                    $product = $this->ask($input, $output, "<fg=yellow>Enter product's name:  </>");
+                    $product = $this->ask($input, $output, "<fg=yellow>Enter product's name: </>", productExists: true);
                     if ($actions->remove($product)) {
                         $output->writeln('<success>Successful removed</>');
                     }
                     break;
+
                 case ActionsType::ADD->value:
                 case ActionsType::CHANGE->value:
-                    $product = $this->ask($input, $output, "<fg=yellow>Enter product's name: </>");
+
+                    $productExists = $inputAction === ActionsType::CHANGE->value;
+                    $product = $this->ask($input, $output, "<fg=yellow>Enter product's name: </>", productExists: $productExists);
                     $price = (int)$this->ask($input, $output, "<fg=yellow>Enter product's price: </>", intValidate: true);
+
                     if ($inputAction === ActionsType::ADD->value && $actions->add($product, $price)) {
                         $output->writeln('<success>Successful added</>');
                     } elseif ($inputAction === ActionsType::CHANGE->value && $actions->change($product, $price)) {
                         $output->writeln('<success>Successful changed</>');
                     }
                     break;
+
                 default:
                     throw new RuntimeException('Wrong action, use: add, remove, change, calculate');
             }
+
         } catch (RuntimeException $e) {
             $output->writeln(sprintf("<error>%s</error>", $e->getMessage()));
             return Command::FAILURE;
         }
+
         return Command::SUCCESS;
     }
 
-    private function ask(InputInterface $input, OutputInterface $output, $question, bool $intValidate = false)
+    private function ask(InputInterface $input, OutputInterface $output, $question, bool $intValidate = false, bool $productExists = null)
     {
         $helper = $this->getHelper('question');
         $question = new Question($question);
+
         if ($intValidate) {
-            $question->setValidator(function (string $answer) {
-                if (!preg_match('#\d#', $answer)) {
+            $validator = static function (string $answer) {
+                if (!preg_match('/^-?\d+$/', $answer)) {
                     throw new RuntimeException('Please enter numeric');
                 }
                 return (int)$answer;
-            });
+            };
         }
+
+        if ($productExists === true) {
+            $validator = function (string $answer) use ($input) {
+                if (!$this->checkProduct($input, $answer)) {
+                    throw new RuntimeException('This product does not exists');
+                }
+                return $answer;
+            };
+        } elseif ($productExists === false) {
+            $validator = function (string $answer) use ($input) {
+                if ($this->checkProduct($input, $answer)) {
+                    throw new RuntimeException('This product is already exists');
+                }
+                return $answer;
+            };
+        }
+
+        if (isset($validator)) {
+            $question->setValidator($validator);
+        }
+
         return $helper->ask($input, $output, $question);
+    }
+
+
+    private function checkProduct(InputInterface $input, $answer): bool
+    {
+        return str_contains(file_get_contents($input->getArgument('filename')), $answer);
     }
 
 }
